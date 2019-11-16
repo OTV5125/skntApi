8caf67382c721b8db7932927496ab3cdc46691b6
OTV <OTV5125@gmail.com>
2019-11-16 14:23:10 +0300

    EDIT Mysql

--- api/Mysql/Mysql.php
+++ api/Mysql/Mysql.php
@@ -10,13 +10,14 @@ namespace Api\Mysql;
 
 use PDO;
 
-class Mysql
+abstract class Mysql
 {
 
-    public $database;
-    public $tableName;
+    protected $pdo;
 
-
+    /**
+     * Mysql constructor.
+     */
     public function __construct()
     {
         require_once 'db_cfg.php';
@@ -30,6 +31,11 @@ class Mysql
         $this->pdo = new PDO($dsn, DB_USER, DB_PASSWORD, $opt);
     }
 
+    /**
+     * @param $sql
+     * @param $param
+     * @return bool|\PDOStatement
+     */
     public function exec($sql, $param)
     {
         if (!is_array($param) || !is_string($sql)) {
@@ -45,35 +51,4 @@ class Mysql
         $stmt->execute();
         return $stmt;
     }
-
-    /**
-     * @param $userId
-     * @param $serviceId
-     * @return mixed
-     */
-    public function getTariffs($userId, $serviceId)
-    {
-        $sql = "SELECT IF(IFNULL((SELECT ID FROM users WHERE ID = :userId), NULL),
-           (SELECT tarif_id FROM services WHERE ID = :serviceId), -1)";
-        $param = [
-            'userId' => $userId,
-            'serviceId' => $serviceId
-        ];
-        $result = $this->exec($sql, $param)->fetchColumn();
-        if (!is_null($result) && $result !== -1) {
-            $sql = "SELECT * FROM tarifs WHERE tarif_group_id = (SELECT tarif_group_id FROM tarifs WHERE ID = :ID)";
-            $param = [
-                'ID' => $result,
-            ];
-            $tariffs = $this->exec($sql, $param)->fetchAll();
-            return ['result' => 'success', 'tariffs' => $tariffs, 'tariffId' => $result];
-        } elseif (is_null($result)) {
-            return ['result' => 'error', 'message' => 'Service id not found'];
-        } elseif ($result === -1) {
-            return ['result' => 'error', 'message' => 'User not found'];
-        } else {
-            return ['result' => 'error', 'message' => 'Unknown error, refer to support'];
-        }
-    }
-
 }
