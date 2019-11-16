6ff989034e3914f9a73c364184019e06d1c9edac
OTV <OTV5125@gmail.com>
2019-11-16 14:51:40 +0300

    EDIT MysqlUsers(add getTariffId)

--- api/Mysql/MysqlUsers.php
+++ api/Mysql/MysqlUsers.php
@@ -16,7 +16,7 @@ class MysqlUsers extends Mysql
      * @param $serviceId
      * @return mixed
      */
-    public function getTariffs($userId, $serviceId)
+    public function getTariffId($userId, $serviceId)
     {
         $sql = "SELECT IF(IFNULL((SELECT ID FROM users WHERE ID = :userId), NULL),
            (SELECT tarif_id FROM services WHERE ID = :serviceId), -1)";
@@ -26,12 +26,7 @@ class MysqlUsers extends Mysql
         ];
         $tariffId = $this->exec($sql, $param)->fetchColumn();
         if (!is_null($tariffId) && $tariffId !== -1) {
-            $sql = "SELECT * FROM tarifs WHERE tarif_group_id = (SELECT tarif_group_id FROM tarifs WHERE ID = :ID)";
-            $param = [
-                'ID' => $tariffId,
-            ];
-            $tariffs = $this->exec($sql, $param)->fetchAll();
-            return ['result' => 'success', 'tariffs' => $tariffs, 'tariffId' => $tariffId];
+           return ['result' => 'ok', 'tariffId' => $tariffId];
         } elseif (is_null($tariffId)) {
             return ['result' => 'error', 'message' => 'Service id not found'];
         } elseif ($tariffId === -1) {
@@ -39,5 +34,18 @@ class MysqlUsers extends Mysql
         } else {
             return ['result' => 'error', 'message' => 'Unknown error, refer to support'];
         }
+    }
+
+    public function getTariffs($userId, $serviceId){
+        $result = $this->getTariffId($userId, $serviceId);
+        if($result['result'] === 'error'){
+            return $result;
+        }
+        $sql = "SELECT * FROM tarifs WHERE tarif_group_id = (SELECT tarif_group_id FROM tarifs WHERE ID = :ID)";
+        $param = [
+            'ID' => $result['tariffId'],
+        ];
+        $tariffs = $this->exec($sql, $param)->fetchAll();
+        return ['result' => 'ok', 'tariffs' => $tariffs, 'tariffId' => $result['tariffId']];
     }
 }
